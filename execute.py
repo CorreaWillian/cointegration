e9d5e0365aacef2550576c7678c6ad10b034e58d
CorreaWillian <willianpc12@gmail.com>
2022-08-02 10:26:35 -0300

    create execute
    Create file and class to execute backtest.

--- execute.py
+++ execute.py
@@ -2,16 +2,15 @@ import pandas as pd
 from cointegracao import Cointegration
 from itertools import combinations, permutations
 import time
-import backtest
+from backtest import Executer
 from tqdm.contrib.concurrent import process_map, thread_map # or thread_map
 
 
-global bd, coint, permut
 
 bd = pd.read_excel('BD COMPLETO.xlsx')
 bd.dropna(axis=0, inplace=True)
 bd.set_index('Data', inplace=True)
-# bd = bd.loc['2018-02-27': '2019-03-02']
+bd = bd.loc['2018-02-27': '2019-03-02']
 
 bd = bd.apply(pd.to_numeric)
 
@@ -22,22 +21,3 @@ i_1 = [ ele for ele in i_1 if ele is not None ]
 
 permut = list(combinations(i_1, 2))
 
-def executer(permut=permut, n_workers=7):
-
-    # if __name__ == '__main__':
-
-    # with Pool(7) as p:
-    #     results = p.imap(cointegrated, permut)
-    results = process_map(backtest.backtest, permut, max_workers=n_workers, chunksize=1)
-    # results = [ ele for ele in results if ele is not None]
-
-    return results
- 
-
-# df.loc[df.pair == ('ARZZ3', 'AZUL4')]
-
-# start = time.time()
-
-# results = executer()
-# print(time.time() - start)
-# print(results)
